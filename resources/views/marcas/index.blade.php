15fae9f2e3f157030f1dcfc9e90d93d23dcbda22
Allan <allanguimaraes@outlook.pt>
2025-05-27 19:53:06 -0300

    Ajustes - Inclusao da barra de navegacao

--- resources/views/marcas/index.blade.php
+++ resources/views/marcas/index.blade.php
@@ -1,39 +1,12 @@
-{{-- resources/views/marcas/index.blade.php --}}
-<!DOCTYPE html>
-<html lang="pt-BR">
+@extends('layouts.head')
+@section('title_aba')
+    Marcas
+@endsection
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Gerenciamento de Marcas</title>
-    <!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
 
 <body>
     <header>
-        <nav>
-            <div class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Celulares</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page"
-                                    href="{{ route('modelos.index') }}">Modelos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('marcas.index') }}">Marcas</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        @include('layouts.nav');
     </header>
     <div class="container py-5">
 
@@ -132,10 +105,8 @@
         </div>
     </div>
 
-    <!-- jQuery e Bootstrap Bundle JS -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
-
+ @extends('layouts.footer')
+ @section('footer')
     <script>
         $(function() {
             // inicializa os modais
@@ -191,6 +162,5 @@
             modalMarca.show();
         </script>
     @endif
-</body>
 
-</html>
+    @endsection
